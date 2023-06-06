Simon1511/android_device_samsung_m52xq

Initial m52xq tree
Change-Id: I501a473b3ec31d29bc605380464859d767012852

device.mk
@@ -1,0 +1,44 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_PATH := device/samsung/m52xq
+
+DEVICE_PACKAGE_OVERLAYS += $(DEVICE_PATH)/overlay
+
+# call the common setup
+$(call inherit-product, device/samsung/sm7325-common/common.mk)
+
+# call the proprietary setup
+$(call inherit-product, vendor/samsung/m52xq/m52xq-vendor.mk)
+
+# Init files
+PRODUCT_PACKAGES += \
+    init.m52xq.rc
+
+# Audio
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
+
+# Recovery modules and firmware
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/recovery/root/vendor/lib/modules/sec_cmd.ko:root/vendor/lib/modules/sec_cmd.ko \
+    $(DEVICE_PATH)/recovery/root/vendor/lib/modules/sec_common_fn.ko:root/vendor/lib/modules/sec_common_fn.ko \
+    $(DEVICE_PATH)/recovery/root/vendor/lib/modules/sec_secure_touch.ko:root/vendor/lib/modules/sec_secure_touch.ko \
+    $(DEVICE_PATH)/recovery/root/vendor/lib/modules/sec_tclm_v2.ko:root/vendor/lib/modules/sec_tclm_v2.ko \
+    $(DEVICE_PATH)/recovery/root/vendor/lib/modules/sec_tsp_dumpkey.ko:root/vendor/lib/modules/sec_tsp_dumpkey.ko \
+    $(DEVICE_PATH)/recovery/root/vendor/lib/modules/sec_tsp_log.ko:root/vendor/lib/modules/sec_tsp_log.ko \
+    $(DEVICE_PATH)/recovery/root/vendor/lib/modules/stm_ts.ko:root/vendor/lib/modules/stm_ts.ko \
+    $(DEVICE_PATH)/recovery/root/vendor/firmware/tsp_stm/fts5cu56a_a52sxq.bin:root/vendor/firmware/tsp_stm/fts5cu56a_a52sxq.bin

lineage_m52xq.mk
@@ -1,0 +1,48 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/m52xq/device.mk)
+
+# Inherit from the 64 bit configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := lineage_m52xq
+PRODUCT_DEVICE := m52xq
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-M526BR
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+PRODUCT_SHIPPING_API_LEVEL := 30
+
+# Vendor fingerprint
+BUILD_FINGERPRINT := "samsung/m52xqxx/m52xq:11/RP1A.200720.012/M526BRXXU2CWB3:user/release-keys"
+PRIVATE_BUILD_DESC := "m52xqxx-user 11 RP1A.200720.012 M526BRXXU2CWB3 release-keys"

rootdir/Android.mk
@@ -1,0 +1,11 @@
+LOCAL_PATH := $(call my-dir)
+
+# Init files
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.m52xq.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.a52sxq.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)

PixelExperience-Devices/device_samsung_m52xq

device.mk
@@ -1,0 +1,46 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_PATH := device/samsung/m52xq
+
+DEVICE_PACKAGE_OVERLAYS += $(DEVICE_PATH)/overlay
+
+# call the common setup
+$(call inherit-product, device/samsung/sm7325-common/common.mk)
+
+# call the proprietary setup
+$(call inherit-product, vendor/samsung/m52xq/m52xq-vendor.mk)
+
+# Init files
+PRODUCT_PACKAGES += \
+    init.m52xq.rc
+
+# Audio
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
+    $(DEVICE_PATH)/configs/audio/audio_platform_info_diff.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_diff.xml
+
+# Recovery modules and firmware
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/recovery/root/vendor/lib/modules/sec_cmd.ko:root/vendor/lib/modules/sec_cmd.ko \
+    $(DEVICE_PATH)/recovery/root/vendor/lib/modules/sec_common_fn.ko:root/vendor/lib/modules/sec_common_fn.ko \
+    $(DEVICE_PATH)/recovery/root/vendor/lib/modules/sec_secure_touch.ko:root/vendor/lib/modules/sec_secure_touch.ko \
+    $(DEVICE_PATH)/recovery/root/vendor/lib/modules/sec_tclm_v2.ko:root/vendor/lib/modules/sec_tclm_v2.ko \
+    $(DEVICE_PATH)/recovery/root/vendor/lib/modules/sec_tsp_dumpkey.ko:root/vendor/lib/modules/sec_tsp_dumpkey.ko \
+    $(DEVICE_PATH)/recovery/root/vendor/lib/modules/sec_tsp_log.ko:root/vendor/lib/modules/sec_tsp_log.ko \
+    $(DEVICE_PATH)/recovery/root/vendor/lib/modules/synaptics_ts.ko:root/vendor/lib/modules/synaptics_ts.ko \
+    $(DEVICE_PATH)/recovery/root/vendor/firmware/tsp_synaptics/s3908_m52xq_boe.bin:root/vendor/firmware/tsp_synaptics/s3908_m52xq_boe.bin \
+    $(DEVICE_PATH)/recovery/root/vendor/firmware/tsp_synaptics/s3908_m52xq_sdc.bin:root/vendor/firmware/tsp_synaptics/s3908_m52xq_sdc.bin

lineage_m52xq.mk
@@ -1,0 +1,48 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/m52xq/device.mk)
+
+# Inherit from the 64 bit configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := lineage_m52xq
+PRODUCT_DEVICE := m52xq
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-M526B
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+PRODUCT_SHIPPING_API_LEVEL := 30
+
+# Vendor fingerprint
+BUILD_FINGERPRINT := "samsung/m52xqins/m52xq:11/RP1A.200720.012/M526BXXS2CWE3:user/release-keys"
+PRIVATE_BUILD_DESC := "m52xqins-user 11 RP1A.200720.012 M526BXXS2CWE3 release-keys"

rootdir/Android.mk
@@ -1,0 +1,11 @@
+LOCAL_PATH := $(call my-dir)
+
+# Init files
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.m52xq.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.m52xq.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)