591e82b068ba5ebf3c1b5f566c7a16378deba1e8
Simon1511 <me@simon1511.de>
2024-01-04 16:19:02 +0100

    m52xq: update everything to M526BXXU3CWH4
    Change-Id: I9911164b77c8ee544d0ba4ad29c9daf78c42cbe3

--- extract-files.sh
+++ extract-files.sh
@@ -14,8 +14,8 @@ function blob_fixup() {
         vendor/lib64/hw/com.qti.chi.override.so)
             xxd -p "${2}" | tr -d \\n > "${2}".hex
             # NOP CONNECT_RILD
-            sed -i "s/800640f92779009480010034a2dbffd0c3ddfff0c5dcff90e603002a/1f2003d51f2003d51f2003d51f2003d51f2003d51f2003d51f2003d5/g" "${2}".hex
-            sed -i "s/427c1b9163100891a5801891a4118052e0031f2a210080528dfeff97800640f9/1f2003d51f2003d51f2003d51f2003d51f2003d51f2003d51f2003d5800640f9/g" "${2}".hex
+            sed -i "s/800640f92979009480010034a2dbffd0c3ddfff0c5dcff90e603002a/1f2003d51f2003d51f2003d51f2003d51f2003d51f2003d51f2003d5/g" "${2}".hex
+            sed -i "s/42f81b91638c0891a5fc1891a4118052e0031f2a210080528dfeff97800640f9/1f2003d51f2003d51f2003d51f2003d51f2003d51f2003d51f2003d5800640f9/g" "${2}".hex
             xxd -r -p "${2}".hex > "${2}"
             rm "${2}".hex
             ;;
